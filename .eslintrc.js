d1b9ff2c7613ec242770051ee83da9e712a8ef3c
kento.muraoka <contact@akoarum.net>
2018-12-05 17:45:11 +0900

    update eslint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
     "vue/max-attributes-per-line": "off",
     "vue/require-default-prop": "off",
     "vue/singleline-html-element-content-newline": "off",
+    "vue/component-name-in-template-casing": "off",
     "prettier/prettier": ["error", {
       "semi": false,
       "bracketSpacing": true,
