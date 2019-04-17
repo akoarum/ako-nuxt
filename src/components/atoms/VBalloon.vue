f9ebe4f86f716a1fab3c1d0a7f2134ce118f9788
kento.muraoka <contact@akoarum.net>
2019-04-17 15:32:04 +0900

    update VBalloon style

--- src/components/atoms/VBalloon.vue
+++ src/components/atoms/VBalloon.vue
@@ -15,7 +15,13 @@ export default {}
   color: #fff;
 
   @include media-min {
+    box-shadow: 0 4px 6px 0 rgba(0, 0, 0, 0.34);
     padding: 8px 16px;
+  }
+
+  @include media-max {
+    box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.48);
+    padding: 9px 13px;
   }
 }
 </style>
