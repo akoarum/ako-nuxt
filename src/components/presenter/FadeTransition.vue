829e4819640a8154cfd3f1c38b13b0339051da4c
kento.muraoka <contact@akoarum.net>
2019-04-17 13:09:20 +0900

    add FadeTransition

---
+++ src/components/presenter/FadeTransition.vue
@@ -0,0 +1,20 @@
+<template>
+  <transition name="fade">
+    <slot />
+  </transition>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s $easing;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- stories/presenter.js
+++ stories/presenter.js
@@ -10,8 +10,30 @@ import {
 } from '@storybook/addon-knobs'
 import { withInfo } from 'storybook-addon-vue-info'
 import StoryRouter from 'storybook-router'
+import FadeTransition from '~/components/presenter/FadeTransition'
 
 storiesOf('Presenter', module)
   .addDecorator(withInfo)
   .addDecorator(withKnobs)
   .addDecorator(StoryRouter({}))
+  .add('FadeTransition', () => ({
+    components: { FadeTransition },
+    template: `
+      <div>
+        <button type="button" @click="toggle">試す</button>
+        <FadeTransition>
+          <p v-show="isVisible">吾輩は猫である</p>
+        </FadeTransition>
+      </div>
+    `,
+    data() {
+      return {
+        isVisible: false
+      }
+    },
+    methods: {
+      toggle() {
+        this.isVisible = !this.isVisible
+      }
+    }
+  }), { info: {} })
