1a8b26012d8c305458f9e0b801fdef168a4fc325
kento.muraoka <contact@akoarum.net>
2019-05-15 14:48:58 +0900

    add VButton component

--- stories/atoms.js
+++ stories/atoms.js
@@ -11,8 +11,24 @@ import {
 } from '@storybook/addon-knobs'
 import { withInfo } from 'storybook-addon-vue-info'
 import StoryRouter from 'storybook-router'
+import VButton from '~/components/atoms/VButton/index'
 
 storiesOf('Atoms', module)
   .addDecorator(withInfo)
   .addDecorator(withKnobs)
   .addDecorator(StoryRouter({}))
+  .add('VButton', () => ({
+    components: { VButton },
+    props: {
+      label: { default: text('ラベル', 'ボタン') },
+      secondary: { default: boolean('secondary', false) },
+      caution: { default: boolean('caution', false) },
+      small: { default: boolean('small', false) }
+    },
+    template: `
+      <VButton :secondary="secondary" :caution="caution" :small="small" @click="click">{{ label }}</VButton>
+    `,
+    methods: {
+      click: action('click')
+    }
+  }), { info: {} })
---
+++ tests/components/atoms/VButton.spec.js
@@ -0,0 +1,28 @@
+import { shallowMount } from '@vue/test-utils'
+import VButton from '~/components/atoms/VButton/index'
+
+describe('VButton', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallowMount(VButton, {
+      propsData: {}
+    })
+  })
+
+  describe('initialized', () => {
+    it('mountable', () => {
+      expect(wrapper.isVueInstance()).toBe(true)
+    })
+    it('snapshot', () => {
+      expect(wrapper.element).toMatchSnapshot()
+    })
+  })
+
+  describe('computed', () => {
+    it('styledClasses: propsからクラス配列を生成して返す', () => {
+      wrapper.setProps({ secondary: true, small: true })
+      expect(wrapper.vm.styledClasses).toEqual(['-secondary', '-small'])
+    })
+  })
+})
