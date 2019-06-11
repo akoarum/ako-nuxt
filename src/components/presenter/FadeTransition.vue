e47c374c3d82c1fb1e8d2d84cba5074328ef49c6
kento.muraoka <contact@akoarum.net>
2019-06-11 17:32:47 +0900

    add FadeTransition component

---
+++ src/components/presenter/FadeTransition.vue
@@ -0,0 +1,22 @@
+<template>
+  <transition name="fade">
+    <slot />
+  </transition>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s $easing;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- stories/presenter.js
+++ stories/presenter.js
@@ -10,8 +10,15 @@ import {
 } from '@storybook/addon-knobs'
 import { withInfo } from 'storybook-addon-vue-info'
 import StoryRouter from 'storybook-router'
+import FadeTransition from '~/components/presenter/FadeTransition'
 
 storiesOf('Presenter', module)
   .addDecorator(withInfo)
   .addDecorator(withKnobs)
   .addDecorator(StoryRouter({}))
+  .add('FadeTransition', () => ({
+    components: { FadeTransition },
+    template: `
+      <FadeTransition />
+    `
+  }), { info: {} })
